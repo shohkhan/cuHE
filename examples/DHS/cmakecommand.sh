79afee3b9f313e446a2a1c13b2201859ab7b4b7a
shohkhan <shoaibapece@gmail.com>
2017-12-27 16:14:57 -0600

    Update cmakecommand.sh

--- examples/DHS/cmakecommand.sh
+++ examples/DHS/cmakecommand.sh
@@ -1,2 +1,2 @@
+rm -rf libdhsShared.so libdhs.a CMakeCache.txt Makefile cmake_install.cmake CMakeFiles
 cmake ./ -DGPU_ARCH:STRING=61 -DGCC_CUDA_VERSION:STRING=gcc-5
-rm -rf libdhsShared.so libdhs.a CMakeCache.txt Makefile CMakeFiles
